d350f66a2811c0a28e9e0e5f28937073275e5e78
Thomas Maurice <thomas@maurice.fr>
2016-01-22 19:31:50 +0100

    Répare la commande cederom, et enlève tableau, qu'elle remplace aventagueusement

--- marcel.py
+++ marcel.py
@@ -22,11 +22,10 @@ TRANSLATIONS = {
     u'construis': 'build',
     u'charge': 'load',
     u'plagie': 'copy',
-    u'tableaux': 'images',
     u'tf1': 'info',
     u'insee': 'stats',
     u'sur-ecoute': u'attach',
-    u'images': u'cederoms'
+    u'cederoms': u'images'
     u'vos-papiers': 'login',
     u'déchéance': 'logout'
 }
